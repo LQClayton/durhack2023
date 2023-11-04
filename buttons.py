ac24e98596c9cccdd2dd3a0d70fe4ac5e43f4a95
LQClayton <lulu.clayton@yahoo.com>
2023-11-04 21:58:41 +0000

    buttons not working

--- buttons.py
+++ buttons.py
@@ -1,3 +1,15 @@
+import taipy as tp
+from taipy import Config, Core, Gui
+
+# A first data node configuration to model an input name.
+input_name_data_node_cfg = Config.configure_data_node(id="input_name")
+# A second data node configuration to model the message to display.
+message_data_node_cfg = Config.configure_data_node(id="message")
+# A task configuration to model the build_message function.
+build_msg_task_cfg = Config.configure_task("build_msg", build_message, input_name_data_node_cfg, message_data_node_cfg)
+# The scenario configuration represents the whole execution graph.
+scenario_cfg = Config.configure_scenario("scenario", task_configs=[build_msg_task_cfg])
+
 def file_handle(filename):
     with open(filename, 'r') as f:
         x = f.readlines()
@@ -20,10 +32,25 @@ def make_buttons():
     pages = {}
     for element in myFile1:
         pages[element] = """
-        <|{element}|button|on_action=submit_scenario|>
+        <|{element}|button|on_action=enlarge|>
         """
 
 
+if __name__ == "__main__":
+    ################################################################
+    #            Instantiate and run Core service                  #
+    ################################################################
+    Core().run()
 
+    ################################################################
+    #            Manage scenarios and data nodes                   #
+    ################################################################
+    scenario = tp.create_scenario(scenario_cfg)
 
-brain_no_function()
+    ################################################################
+    #            Instantiate and run Gui service                   #
+    ################################################################
+
+    Gui(pages).run()
+
+make_buttons()
