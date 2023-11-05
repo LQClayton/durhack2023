4f065672c1332c8ac825b952791ea249c74286cc
LQClayton <lulu.clayton@yahoo.com>
2023-11-05 10:14:20 +0000

    Buttons start counting

--- main.py
+++ main.py
@@ -2,22 +2,22 @@ import tkinter as tk
 import time
 
 window = tk.Tk()
-guiDict = {"myButtonsP":"","myButtonsE":"", "lastClicked":""}
+guiDict = {"myButtonsP":"","myButtonsE":"", "panic":""}
 
-def counting():
-		#while page is running
-		for i in range(5):
-				print(i+1)
-				time.sleep(1)
+def counting(event):
+		for j in range(5):
+				for i in range(5):
+						print(i+1)
+						time.sleep(1)
 
 def rhythm():
-		#use with buttons
+		#use with buttons - vibrate if you tap the right ones in a certain amount of time
 		pass
 
 def enlarge(event):
+		#need some way to take in button information ie text and change the text size
 		print(event)
-		
-
+	
 def file_handle(filename):
 		with open(filename, 'r') as f:
 				x = f.readlines()
@@ -36,13 +36,18 @@ def brain_no_function():
 		print(myFile1, myFile2)
 
 def make_buttons(myFile,dictKey):
-		print("Hello World")
 		for i in range(len(myFile)):
 				button = tk.Button(text=myFile[i], width=25, height=5, bg="white", fg="pink",)
 				button.pack()
 				guiDict[dictKey] = button
-				button.bind("<Button>", enlarge)
+				button.bind("<Button>", counting)
 		button.pack()
+		loop()
+
+def make_panic():
+		guiDict["panic"] = tk.Button(text="I need to breathe", width=25, height=5, bg="white", fg="pink",)
+		guiDict["panic"].pack()
+		guiDict["panic"].bind("<Button>", counting)
 		loop()
 
 def loop():
@@ -50,7 +55,10 @@ def loop():
 
 def display():
 		phraseMake = file_handle("phrases.txt")
+		emotionsMake = file_handle("emotions.txt")
 		make_buttons(phraseMake, "myButtonsP")
+		make_buttons(emotionsMake, "myButtonsE")
+		make_panic()
 		
 
 display()
